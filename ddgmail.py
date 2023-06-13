2b3494770d3b2fd6d44d27680e1594fd41930b7d
rany <ranygh@riseup.net>
2023-06-13 14:49:29 +0300

    Add command alias support
    `ddgmail d` matches `ddgmail dashboard` now, and so on..
    
    Signed-off-by: rany <ranygh@riseup.net>

--- ddgmail.py
+++ ddgmail.py
@@ -32,6 +32,24 @@ os.makedirs(config_dir, exist_ok=True)
 config_file = os.path.join(config_dir, "config.json")
 
 
+class AliasedGroup(click.Group):
+    def get_command(self, ctx, cmd_name):
+        rv = click.Group.get_command(self, ctx, cmd_name)
+        if rv is not None:
+            return rv
+        matches = [x for x in self.list_commands(ctx) if x.startswith(cmd_name)]
+        if not matches:
+            return None
+        elif len(matches) == 1:
+            return click.Group.get_command(self, ctx, matches[0])
+        ctx.fail(f"Too many matches: {', '.join(sorted(matches))}")
+
+    def resolve_command(self, ctx, args):
+        # always return the full command name
+        _, cmd, args = super().resolve_command(ctx, args)
+        return cmd.name, cmd, args
+
+
 def load_config():
     config = None
 
@@ -55,7 +73,7 @@ def save_config(config):
         json.dump(config, fobj)
 
 
-@click.group()
+@click.command(cls=AliasedGroup)
 def cli():
     pass
 
